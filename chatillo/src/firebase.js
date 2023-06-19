b1133b37bec52866b69c814a096cbe37be6e79fb
dirky7 <dhurper0905@g.educaand.es>
2023-06-19 23:39:34 +0200

    chatillo realease 1.4

--- chatillo/src/firebase.js
+++ chatillo/src/firebase.js
@@ -4,13 +4,6 @@ import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-	apiKey: "AIzaSyAio9PRplolRuvyLi_O2D1narsXCqUjiR8",
-	authDomain: "chatillo-442e9.firebaseapp.com",
-	projectId: "chatillo-442e9",
-	storageBucket: "chatillo-442e9.appspot.com",
-	messagingSenderId: "789473032322",
-	appId: "1:789473032322:web:507895bc50a219b4fadc9b",
-	measurementId: "G-PTVKXNW23F"
 };
 
 export const app = initializeApp(firebaseConfig);
